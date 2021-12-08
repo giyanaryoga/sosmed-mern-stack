acf509cd53c9abbefa99006e9d384d3580219fba
Giyanaryoga Puguh SP <giyanaryoga@gmail.com>
2021-12-08 22:28:31 +0700

    update posts

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
-
 import { reducers } from "./reducers/index";
+import "./index.css";
 import App from "./App";
 
 const store = createStore(reducers, compose(applyMiddleware(thunk)));
